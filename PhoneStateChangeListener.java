7233cff8ff8fd4d36c0483332c76c0b89e6e29ed
Stefano Stypulkowski <made.in.stefano@gmail.com>
2013-04-24 17:58:44 -0300

    initial release

---
+++ PhoneStateChangeListener.java
@@ -0,0 +1,105 @@
+/**
+ * 	PhoneListener.java
+ * 	PhoneListener PhoneGap plugin (Android)
+ * 
+ * 	@author Stéfano Stypulkows Zanata
+ * 	@see http://szanata.com 
+ * 	@licence MIT <http://szanata.com/MIT.txt>
+ * 	@licence GNU <http://szanata.com/GNU.txt>
+ * 	Based upon PhoneListener by authored by Tommy-Carlos Williams <https://github.com/devgeeks> and updated by Matt McGrath
+ * 
+ *
+ */
+package com.szanata.cordova.plugins;
+
+import org.apache.cordova.api.CallbackContext;
+import org.apache.cordova.api.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+/**
+ * 
+ *
+ */
+public class PhoneStateChangeListener extends CordovaPlugin {
+
+	private static final String LOG_TAG = "PhoneListener";
+	private Context context;
+	private CallbackContext callbackContext;
+	private BroadcastReceiver receiver = null;
+	
+	/**
+	 * Expecting no parameters from javascript
+	 * Expecting only success callback from javascript
+	 */
+	@Override
+	public boolean execute(final String action,final JSONArray args, final CallbackContext callbackContext) throws JSONException {	
+		
+		this.context = cordova.getActivity().getApplicationContext();
+		
+		if ("start".equals(action)) {
+			this.callbackContext = callbackContext;
+			startPhoneListener();
+			return true;
+			
+		}else if ("stop".equals(action)) {
+			removePhoneListener();
+			this.callbackContext = null;
+			return true;
+		}
+		
+		return false;
+	}
+
+	/**
+	 * creates a new BroadcastReceiver to listen whether the Telephony State changes
+	 */
+	public void startPhoneListener() {
+		
+		if (this.receiver == null) {
+			this.receiver = new BroadcastReceiver() {
+
+				@Override
+				public void onReceive(final Context context, final Intent intent) {	
+
+					if(intent != null && intent.getAction().equals(TelephonyManager.ACTION_PHONE_STATE_CHANGED)) {
+						String state = intent.hasExtra(TelephonyManager.EXTRA_STATE) ? intent.getStringExtra(TelephonyManager.EXTRA_STATE) : "NONE";
+			            
+			            if (callbackContext != null){
+			            	callbackContext.success(state);
+			            }
+					}
+					
+				}
+			};
+			this.context.registerReceiver(this.receiver, new IntentFilter(TelephonyManager.ACTION_PHONE_STATE_CHANGED));
+		}
+	}
+	
+	
+	/**
+     * removes the Receiver
+     */
+    private void removePhoneListener() {
+        if (this.receiver != null) {
+            try {
+                this.context.unregisterReceiver(this.receiver);
+                this.receiver = null;
+            } catch (final Exception e) {
+                Log.e(LOG_TAG, "Error unregistering phone listener receiver: " + e.getMessage(), e);
+            }
+        }
+    }
+	
+
+	public void onDestroy() {
+		removePhoneListener();
+	}
+}
--- README.md
+++ README.md
@@ -2,3 +2,30 @@ PhoneStateChangeListener
 ========================
 
 Cordova plugin to provide callback when Telephony state changes
+
+Possible states are IDLE, OFFHOOK and RINGING
+
+# to use:
+
+1 - Import the plugin right after your cordova.js file
+2 - Setup the callback to handle when state changes:
+    
+    plugins.PhoneStateChangeListener.start(function (state){
+      // do stuff with the state
+    });
+    
+3 - Possible values to 'state' variable are:
+
+    plugins.PhoneStateChangeListener.IDLE // when the none call are being made
+    plugins.PhoneStateChangeListener.RINGING // when the phone is ringing
+    plugins.PhoneStateChangeListener.OFFHOOK // when the phone is busy
+    plugins.PhoneStateChangeListener.NONE // when something goes wrong
+    
+4 - To stop listening to this:
+
+    plugins.PhoneStateChangeListener.stop();
+    
+    
+# version 1
+- Added basic support to listen when Telephony state changes
+- Added feature to remove this listener
